5919af37885ca1805ef62d4a4d0558d914c38144
BJORN-VINTHER <97097933+BJORN-VINTHER@users.noreply.github.com>
2022-11-06 20:14:24 +0100

    Fixed links page

---
+++ js/scroller.js
@@ -0,0 +1,7 @@
+Reveal.on('slidechanged', event => {
+    // event.previousSlide, event.currentSlide, event.indexh, event.indexv
+    if (event.indexhd === 5) {
+        console("Hit");
+        
+    }
+  } );
